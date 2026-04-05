e3e892d6db3df8333bd526342e740ae20180af8c
Nicolás Peñalba <matador50200@gmail.com>
2026-04-04 23:30:03 -0300

    Update incisoF
    Corregir inconsistencias con comentarios

--- C/IncisoF.c
+++ C/IncisoF.c
@@ -11,10 +11,10 @@ El programa debe validar la entrada y determinar si el número ingresado está d
 
 int IncisoF() {
     char input[50]; /* el número se almacena como un array de caracteres */
-    int signo = 1; /*valor para representar el signo */
+    int signo = 1; /* valor para representar el signo */
     int entera = 0; /* variable para representar la parte entera del número */
     int fraccion = 0; /* variable para representar la parte fraccionaria del número */
-    int divisor = 1; /*variable para guardar el divisor de la parte fraccionaria */
+    int divisor = 1; /* variable para guardar el divisor de la parte fraccionaria */
 
     printf("Ingrese un numero (+/-eee.ffff): ");
 
@@ -41,7 +41,7 @@ int IncisoF() {
         return 1;
     }
 
-    /* a medida que lee digitos, los va metiendo en la parte entera (añade digito a digito multiplicando por 10) (resta el '0' porque son caracteres)*/
+    /* a medida que lee digitos, los va metiendo en la parte entera (añade digito a digito multiplicando por 10) (resta el '0' porque son caracteres) */
     while (isdigit(input[i]) && i<sizeof(input)) {
         entera = entera * 10 + (input[i] - '0');
         i++;
@@ -62,10 +62,10 @@ int IncisoF() {
         }
     }
 
-    /*convierte la parte entera a punto flotante multiplicando por la escala (2^8)*/
+    /* convierte la parte entera a punto flotante multiplicando por la escala (2^8) */
     int resultado = entera * ESCALA;
 
-    /*realiza el mismo proceso con la parte fraccionaria (para mejorar precisión realiza un redondeo sumando el divisor/2 al numero antes de hacer la división, ya que si lo haces directo algunos números no se representan bien)*/
+    /* realiza el mismo proceso con la parte fraccionaria (para mejorar precisión realiza un redondeo sumando el divisor/2 al numero antes de hacer la división, ya que si lo haces directo algunos números no se representan bien) */
     if (divisor > 1) {
         int num = fraccion * ESCALA;
 
@@ -75,22 +75,22 @@ int IncisoF() {
         else
             num -= divisor / 2;
 
-        /*se suma la parte fraccionaria al resultado (para convertirla de entero a decimales se divide por el divisor)*/
+        /* se suma la parte fraccionaria al resultado (para convertirla de entero a decimales se divide por el divisor) */
         resultado += num / divisor;
     }
-    /*multiplica por +-1 para darle signo al numero*/
+    /* multiplica por +-1 para darle signo al numero */
     resultado *= signo;
 
-    /*chequeo*/
+    /* chequeo */
     printf("%d\n", resultado);
 
-    /*valida el rango (los numeros ahí equivalen a -128 y 127 en decimal respectivamente)*/
+    /* valida el rango (los numeros ahí equivalen a -128 y 127 en decimal respectivamente) */
     if (resultado < -32768 || resultado > 32767) {
         printf("Error: fuera de rango\n");
         return 1;
     }
 
-    /*imprime el resultado (se debe usar unsigned int ya que si no se pisan los signos - y el número se imprime incorrectamente)*/
+    /* imprime el resultado (se debe usar unsigned int ya que si no se pisan los signos - y el número se imprime incorrectamente) */
     printf("Resultado Q(7,8): 0x%04X\n",(uint16_t) resultado);
 
     return 0;
