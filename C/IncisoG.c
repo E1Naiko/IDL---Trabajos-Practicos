8af39485c0238897917fb2e2c27f66182fe53b7d
Selgoku <99047802+Selgoku@users.noreply.github.com>
2026-04-07 20:25:25 -0300

    Actualizado incisoG

--- C/IncisoG.c
+++ C/IncisoG.c
@@ -14,23 +14,24 @@ int hexToInt(char c);/*No se usa en esta implementacion*/
 
 int IncisoG(){
     uint16_t valor = 0;
-    printf("\nIngrese un numero en formato 0xHHHH: "); 
-    
+    printf("\nIngrese un numero en formato 0xHHHH: ");
+
     /* Leer directamente hexadecimal */
-    if (scanf("%hx", &valor) != 1) {
+    if (scanf("%4hx", &valor) != 1) {
         printf("\nFormato invalido");
+        getchar(); /* Limpiar el buffer de entrada */
         return 1;
     }
-    
+
     /*Mostrar el valor entero sin signo para verificar la conversión*/
-    printf("\nValor entero sin signo: %u", valor); 
-    
+    printf("\nValor entero sin signo: %u", valor);
+
     /* Interpretar como int16_t (complemento a 2)*/
-    int16_t valor16 = (int16_t)valor; 
-    
+    int16_t valor16 = (int16_t)valor;
+
     /*Mostrar el valor entero con signo para verificar la interpretación*/
-    printf("\nValor entero con signo: %d", valor16); 
-    
+    printf("\nValor entero con signo: %d", valor16);
+
     /* Separar partes
 
     Formato Q(7,8):
@@ -38,17 +39,17 @@ int IncisoG(){
    - 1 bit de signo
    - 7 bits parte entera
    - 8 bits parte fraccionaria
-   - Valor real = numero / 256 
+   - Valor real = numero / 256
    */
-    
+
     int signo = 1; /*Valor para representar el signo, se asume positivo por defecto*/
     int32_t abs_val = valor16; /*Convertir a valor absoluto para facilitar el cálculo de la parte entera y fraccionaria*/
-    
-    if (valor16 < 0) { 
+
+    if (valor16 < 0) {
         signo = -1;
         abs_val = -valor16; /*Si el número es negativo, se cambia el signo y se obtiene el valor absoluto "cambiado"*/
     }
-    
+
     int entero = abs_val / 256; /*La parte entera se obtiene dividiendo el valor absoluto por 256,
     ya que en formato Q(7,8) la parte entera ocupa 7 bits y la parte fraccionaria ocupa 8 bits, lo que equivale a dividir por 2^8 = 256
     Basicamente la inversa de hacer resultado = entero * escala del conversor de decimal a hexa */
@@ -60,7 +61,7 @@ int IncisoG(){
     fraccion = (resto / 256) ≈ decimal
     (resto * 10000) / 256 -> convierte a entero con 4 cifras decimales
     Ej: 0.5 -> 5000 -> se imprime como 0.5000 */
-    int fraccion = (resto * 10000) /256; 
+    int fraccion = (resto * 10000) /256;
 
 
     /* Mostrar signo negativo*/
@@ -69,7 +70,7 @@ int IncisoG(){
     }
 
     printf("\nvalor en formato Q(7,8): %d.%04d", entero, fraccion); /* Mostrar el valor en formato Q(7,8)*/
-
+    getchar(); /* Limpiar el buffer de entrada */
     return 0;
 }
 
@@ -85,7 +86,7 @@ int hexToInt(char c){
     }
     if (c >= 'A' && c <= 'F'){
         return 10 + (c - 'A');/*Si es una letra mayúscula, restamos 'A' y sumamos 10 para obtener su valor numérico*/
-    } 
+    }
     if (c >= 'a' && c <= 'f') {
         return 10 + (c - 'a');/*Si es una letra minúscula, restamos 'a' y sumamos 10 para obtener su valor numérico*/
     }
